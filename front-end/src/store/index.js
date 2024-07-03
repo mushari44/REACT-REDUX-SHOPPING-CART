994202170b1a219079860a2f31386138b3d8bec5
mushari44 <meeshoo-010@hotmail.com>
2024-07-03 17:42:26 +0300

    removed frontEnd folder

--- front-end/src/store/index.js
+++
@@ -1,5 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "./slices/cart-slice";
-
-const store = configureStore({ reducer: { cart: cartReducer } });
-export default store;
